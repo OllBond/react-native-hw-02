29064c024b73cd56572713ddfe3b22b730c91d33
Olena <newsroommy@gmail.com>
2023-02-22 00:23:03 +0300

    add

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -4,7 +4,7 @@ import {
   StyleSheet,
   TextInput,
   ImageBackground,
-  Button,
+  Image,
   TouchableOpacity,
 } from "react-native";
 const RegistrationScreen = () => {
@@ -13,32 +13,56 @@ const RegistrationScreen = () => {
       <ImageBackground
         style={styles.image}
         source={require("../assets/image/photo-bg.jpg")}
-      ></ImageBackground>
-      <Text style={styles.title}>Регистрация</Text>
-      <View style={styles.form}>
-        <View>
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder="Логин"
-          />
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder="Адрес электронной почты"
-          />
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder="Пароль   Показать"
-            secureTextEntry={true}
+      >
+        <View style={styles.wrapperForm}>
+          <View style={styles.imageWrapper}>
+            <Image
+              style={styles.imageBlack}
+              source={require("../assets/image/black.png")}
+            />
+            <Image
+              style={styles.imageAddPhoto}
+              source={require("../assets/image/add-photo.png")}
+            />
+          </View>
+          <View style={styles.form}>
+            <View>
+              <Text style={styles.title}>Регистрация</Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                placeholder="Логин"
+              />
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                placeholder="Адрес электронной почты"
+              />
+              <View>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  placeholder="Пароль"
+                  secureTextEntry={true}
+                />
+                <Text style={styles.textShow}>Показать</Text>
+              </View>
+            </View>
+            <TouchableOpacity activeOpacity={0.8} style={styles.button}>
+              <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.paragraph}>Уже есть аккаунт? Войти</Text>
+          </TouchableOpacity>
+          <Image
+            style={styles.imageAddPhoto}
+            source={require("../assets/image/home-indicator.png")}
           />
         </View>
-        <TouchableOpacity activeOpacity={0.8} style={styles.button}>
-          <Text style={styles.btnTitle}>Зарегистрироваться</Text>
-        </TouchableOpacity>
-        <Text style={styles.paragraph}>Уже есть аккаунт? Войти</Text>
-      </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -49,38 +73,78 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    // width: 375,
-    height: 812,
-    fontFamily: "Roboto",
   },
+  imageBlack: {
+    position: "absolute",
+    width: 375,
+    height: 44,
+  },
+  // imageAddPhoto: {
+  //   position: "absolute",
+  //   left: "90%",
+  //   top: "65%",
+  //   width: 25,
+  //   height: 25,
+  // },
   title: {
     marginBottom: 33,
+    fontFamily: "Roboto",
     fontSize: 30,
     fontWeight: "500",
+    textAlign: "center",
     lineHeight: 35,
     letterSpacing: 0.01,
+    color: "#212121",
   },
-  // image: {
-  //   flex: 1,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   resizeMode: "cover",
-  //   width: 375,
-  //   height: 812,
-  // },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+    // alignItems: "center",
+    resizeMode: "cover",
+    // width: 375,
+    // height: 812,
+  },
+  imageWrapper: {
+    position: "absolute",
+    left: "35%",
+    top: "-15%",
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+  },
   input: {
     padding: 16,
-    marginBottom: 16,
-    borderWidth: 1,
-    width: 343,
+    marginTop: 16,
     height: 50,
+    fontFamily: "Roboto",
+    fontSize: 16,
+    lineHeight: 19,
     backgroundColor: "#F6F6F6",
     color: "#212121",
     borderColor: "1px solid #E8E8E8",
     borderRadius: 8,
+    borderWidth: 1,
+  },
+  wrapperForm: {
+    paddingTop: 92,
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  textShow: {
+    position: "absolute",
+    top: "50%",
+    left: "78%",
+    fontFamily: "Roboto",
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "right",
+    color: "#1B4371",
   },
   form: {
-    marginHorizontal: 40,
+    marginHorizontal: 16,
   },
   button: {
     backgroundColor: "#FF6C00",
